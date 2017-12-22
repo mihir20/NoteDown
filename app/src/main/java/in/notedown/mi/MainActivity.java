ab3de5f59533f56bf737999ed2febf0de57835db
mihir20 <mihir20121997@gmail.com>
2017-12-22 21:37:09 +0530

    fragments added

--- app/src/main/java/in/notedown/mi/MainActivity.java
+++ app/src/main/java/in/notedown/mi/MainActivity.java
@@ -1,5 +1,6 @@
 package in.notedown.mi;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        HomeFragment homeFragment = new HomeFragment();
+        fragmentManager.beginTransaction()
+                .replace( R.id.fragment_container, homeFragment );
     }
 }
