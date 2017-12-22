90736df827fb325addd6ab9fec7fb77da6f42172
mihir20 <mihir20121997@gmail.com>
2017-12-23 03:44:42 +0530

    save function added

--- app/src/main/java/in/notedown/mi/WritingFragment.java
+++ app/src/main/java/in/notedown/mi/WritingFragment.java
@@ -2,14 +2,28 @@ package in.notedown.mi;
 
 
 import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.github.gcacace.signaturepad.views.SignaturePad;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 
 /**
@@ -28,9 +42,9 @@ public class WritingFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate( R.layout.fragment_writing, container, false );
 
-        final SignaturePad writingPad = (SignaturePad) rootView.findViewById( R.id.writingPad );
-        FloatingActionButton eraseFAB = (FloatingActionButton) rootView.findViewById( R.id.erase_fab );
-        FloatingActionButton saveFAB = (FloatingActionButton) rootView.findViewById( R.id.save_fab );
+        final SignaturePad writingPad =  rootView.findViewById( R.id.writingPad );
+        FloatingActionButton eraseFAB =  rootView.findViewById( R.id.erase_fab );
+        FloatingActionButton saveFAB =   rootView.findViewById( R.id.save_fab );
 
         eraseFAB.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -39,7 +53,63 @@ public class WritingFragment extends Fragment {
             }
         } );
 
+        saveFAB.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bmp= writingPad.getSignatureBitmap();
+
+                if (addJpgNoteToGallery(bmp)) {
+                    Toast.makeText(getContext(), "Note saved into the Gallery", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), "Unable to store the note", Toast.LENGTH_SHORT).show();
+                }
+
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace( R.id.fragment_container,
+                        new HomeFragment()).commit();
+            }
+        } );
+
         return rootView;
     }
 
+    public File getAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File( Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e("NoteDown", "Directory not created");
+        }
+        return file;
+    }
+
+    public void saveBitmapToJPG(Bitmap bitmap, File photo) throws IOException {
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(newBitmap);
+        canvas.drawColor( Color.WHITE);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        OutputStream stream = new FileOutputStream(photo);
+        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
+        stream.close();
+    }
+
+    public boolean addJpgNoteToGallery(Bitmap note) {
+        boolean result = false;
+        try {
+            File photo = new File(getAlbumStorageDir("NoteDown"), String.format("Note_%d.jpg", System.currentTimeMillis()));
+            saveBitmapToJPG(note, photo);
+            scanMediaFile(photo);
+            Toast.makeText( getContext(),""+photo,Toast.LENGTH_LONG ).show();
+            result = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    private void scanMediaFile(File photo) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(photo);
+        mediaScanIntent.setData(contentUri);
+        this.getActivity().sendBroadcast(mediaScanIntent);
+    }
 }
