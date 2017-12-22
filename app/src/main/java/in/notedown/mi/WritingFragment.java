32aa15dc48f93ae3aff52c1e01ed1d0d64fdbc7c
mihir20 <mihir20121997@gmail.com>
2017-12-23 00:04:14 +0530

    erase function added

--- app/src/main/java/in/notedown/mi/WritingFragment.java
+++ app/src/main/java/in/notedown/mi/WritingFragment.java
@@ -2,10 +2,13 @@ package in.notedown.mi;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.gcacace.signaturepad.views.SignaturePad;
 
 
 /**
@@ -23,7 +26,20 @@ public class WritingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_writing, container, false );
+        View rootView = inflater.inflate( R.layout.fragment_writing, container, false );
+
+        final SignaturePad writingPad = (SignaturePad) rootView.findViewById( R.id.writingPad );
+        FloatingActionButton eraseFAB = (FloatingActionButton) rootView.findViewById( R.id.erase_fab );
+        FloatingActionButton saveFAB = (FloatingActionButton) rootView.findViewById( R.id.save_fab );
+
+        eraseFAB.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writingPad.clear();
+            }
+        } );
+
+        return rootView;
     }
 
 }
